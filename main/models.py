c1b22b365cdbda0d10ead646dab2baa8eaa43797
Osku Salerma <osku.salerma@digital.trade.gov.uk>
2020-09-07 16:19:44 +0100

    Support booking on behalf of someone else.

--- main/models.py
+++ main/models.py
@@ -41,6 +41,9 @@ class Floor(models.Model):
 class Booking(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, models.CASCADE, db_index=True, related_name="+")
 
+    # if non-null, means user booked on behalf of someone else
+    on_behalf_of = models.CharField(max_length=80, blank=True, null=True)
+
     booking_date = models.DateField(db_index=True)
 
     building = models.ForeignKey(Building, on_delete=models.CASCADE, related_name="+")
--- main/widgets.py
+++ main/widgets.py
@@ -21,3 +21,14 @@ class GovUKRadioSelect(forms.RadioSelect):
         ctx.update({"form_instance": self.form_instance})
 
         return ctx
+
+
+class GovUKTextInput(forms.TextInput):
+    template_name = 'govuk/forms/widgets/textinput.html'
+
+    def get_context(self, name, value, attrs):
+        ctx = super().get_context(name, value, attrs)
+
+        ctx.update({"form_instance": self.form_instance})
+
+        return ctx
