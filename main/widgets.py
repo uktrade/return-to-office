fe0f282ce1bf1fa544b043975cca2835f7354625
Osku Salerma <osku.salerma@digital.trade.gov.uk>
2020-09-07 15:37:30 +0100

    Add gov.uk styling for radio buttons.

--- main/widgets.py
+++ main/widgets.py
@@ -1,8 +1,19 @@
 from django import forms
 
 
-class GovUKCheckboxInput(forms.widgets.CheckboxInput):
+class GovUKCheckboxInput(forms.CheckboxInput):
     template_name = 'govuk/forms/widgets/checkbox.html'
+
+    def get_context(self, name, value, attrs):
+        ctx = super().get_context(name, value, attrs)
+
+        ctx.update({"form_instance": self.form_instance})
+
+        return ctx
+
+
+class GovUKRadioSelect(forms.RadioSelect):
+    template_name = 'govuk/forms/widgets/radio.html'
 
     def get_context(self, name, value, attrs):
         ctx = super().get_context(name, value, attrs)
