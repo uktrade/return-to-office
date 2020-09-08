c799d7bf07a3a4c76f4bb8e1746f6273305a7da3
Osku Salerma <osku.salerma@digital.trade.gov.uk>
2020-09-08 12:57:13 +0100

    Implement pingdom health check.

--- pingdom/views.py
+++ pingdom/views.py
@@ -1,3 +1,5 @@
+import time
+
 from django.http import HttpResponse
 
 from .services import services_to_check
@@ -5,22 +7,28 @@ from .services import services_to_check
 PINGDOM_TEMPLATE = """<?xml version="1.0" encoding="UTF-8"?>
 <pingdom_http_custom_check>
     <status>{status}</status>
+    <response_time>{response_time}</response_time>
 </pingdom_http_custom_check>\n"""
 
 COMMENT_TEMPLATE = "<!--{comment}-->\n"
 
 
 def pingdom(request):
+    t = time.time()
     checked = {}
+
     for service in services_to_check:
         checked[service.name] = service().check()
 
+    # pingdom can only accept 3 fractional digits
+    t = "%.3f" % (time.time() - t,)
+
     if all(item[0] for item in checked.values()):
         return HttpResponse(
-            PINGDOM_TEMPLATE.format(status="OK"), content_type="text/xml"
+            PINGDOM_TEMPLATE.format(status="OK", response_time=t), content_type="text/xml"
         )
     else:
-        body = PINGDOM_TEMPLATE.format(status="FALSE")
+        body = PINGDOM_TEMPLATE.format(status="FALSE", response_time=t)
         for service_result in filter(lambda x: x[0] is False, checked.values()):
             body += COMMENT_TEMPLATE.format(comment=service_result[1])
         return HttpResponse(
