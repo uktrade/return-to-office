abd242a7da93ea5895bd5b657d8fb62fbc59a8aa
Osku Salerma <osku.salerma@digital.trade.gov.uk>
2020-09-08 12:57:13 +0100

    Implement pingdom health check.

--- pingdom/services.py
+++ pingdom/services.py
@@ -1,18 +1,17 @@
 from django.db import DatabaseError
 
+from main.models import Building
+
 
 class CheckDatabase:
     name = "database"
 
     def check(self):
-        # TODO: implement pingdom checks
-        return True, ""
-
-        # try:
-        #     FinancialYear.objects.all().exists()
-        #     return True, ""
-        # except DatabaseError as e:
-        #     return False, e
+        try:
+            Building.objects.all().exists()
+            return True, ""
+        except DatabaseError as e:
+            return False, e
 
 
 services_to_check = (CheckDatabase,)
