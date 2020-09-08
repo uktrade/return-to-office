ed105746db356df9b8afc4007b214682de5ce1a6
Osku Salerma <osku.salerma@digital.trade.gov.uk>
2020-09-08 14:29:39 +0100

    Change floors to just have a desk count, not individual desks.

--- main/models.py
+++ main/models.py
@@ -14,28 +14,10 @@ class Floor(models.Model):
 
     building = models.ForeignKey(Building, on_delete=models.CASCADE, related_name="floors")
     name = models.CharField(max_length=20)
-
-    # for now, a newline-separated list of desk IDs
-    desks = models.TextField()
+    nr_of_desks = models.PositiveIntegerField()
 
     def __str__(self):
         return self.name
-
-    def nr_of_desks(self):
-        """Get number of desks."""
-
-        return len(self.desks.split("\n"))
-
-    def get_free_desk(self, used_desks):
-        """Given used_desks (set of used desks), return a random free desk.
-
-        Raises exception if no free desks available."""
-
-        all_desks = set(self.desks.split("\n"))
-
-        free_desks = all_desks - used_desks
-
-        return free_desks.pop()
 
 
 class Booking(models.Model):
@@ -51,7 +33,5 @@ class Booking(models.Model):
     floor = models.ForeignKey(Floor, on_delete=models.CASCADE, related_name="+")
 
     directorate = models.CharField(max_length=80)
-
-    desk = models.CharField(max_length=20)
 
     booked_timestamp = models.DateTimeField(auto_now_add=True)
