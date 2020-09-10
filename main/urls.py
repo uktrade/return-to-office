4930e765774a1897d1cbd1b62b7bbe1415d9da5b
Osku Salerma <osku.salerma@digital.trade.gov.uk>
2020-09-10 15:46:51 +0100

    Add privacy policy.

--- main/urls.py
+++ main/urls.py
@@ -6,6 +6,7 @@ app_name = "main"
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("privacy-policy", views.show_privacy_policy, name="show-privacy-policy"),
     path("my-bookings", views.show_bookings, name="show-bookings"),
     path("booking/create-who-for", views.create_booking_who_for, name="booking-create-who-for"),
     path("booking/create-initial", views.create_booking_initial, name="booking-create-initial"),
