c35ddfe7201602df252ac2aa1690047c4921c94e
Osku Salerma <osku.salerma@digital.trade.gov.uk>
2020-09-10 08:38:04 +0100

    Add "who are you booking for" initial screen on the booking flow.

--- main/urls.py
+++ main/urls.py
@@ -7,6 +7,7 @@ app_name = "main"
 urlpatterns = [
     path("", views.index, name="index"),
     path("my-bookings", views.show_bookings, name="show-bookings"),
+    path("booking/create-who-for", views.create_booking_who_for, name="booking-create-who-for"),
     path("booking/create-initial", views.create_booking_initial, name="booking-create-initial"),
     path("booking/create-finalize", views.create_booking_finalize, name="booking-create-finalize"),
 ]
