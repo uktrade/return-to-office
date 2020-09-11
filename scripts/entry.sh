feab1586ec3d1e42c9128402fddf543992870783
Osku Salerma <osku.salerma@digital.trade.gov.uk>
2020-09-11 15:52:11 +0100

    Create scripts/entry.sh, call it from Procfile.

---
+++ scripts/entry.sh
@@ -0,0 +1,15 @@
+#!/bin/sh -e
+
+python manage.py migrate --noinput
+
+python manage.py compilescss
+
+python manage.py collectstatic --noinput
+
+gunicorn config.wsgi:application \
+  --config config/gunicorn.py \
+  --worker-class gevent \
+  --worker-connections 1000 \
+  --bind 0.0.0.0:$PORT \
+  --timeout 300 \
+  --log-file -
