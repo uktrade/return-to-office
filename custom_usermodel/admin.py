5f769931391b72ce09be9e9ba80962f95bb47a5f
Osku Salerma <osku.salerma@digital.trade.gov.uk>
2020-12-09 08:13:32 +0000

    Show username/contact_email fields on User admin site.

--- custom_usermodel/admin.py
+++ custom_usermodel/admin.py
@@ -9,7 +9,7 @@ from .models import User
 @admin.register(User)
 class UserAdmin(BaseUserAdmin, admin.ModelAdmin):
     fieldsets = (
-        (_("Login Info"), {"fields": ("email", "password")}),
+        (_("Login Info"), {"fields": ("email", "username", "contact_email", "password")}),
         (_("Personal info"), {"fields": ("first_name", "last_name")}),
         (_("Account Status"), {"fields": ("is_active", "is_staff", "is_superuser")}),
         (_("Groups and User Permissions"), {"fields": ("groups", "user_permissions")}),
