7ccf83e5b5c95f60033d91d16bbcc81469cfd979
Ross Miller <github@decodedcollective.com>
2021-05-28 12:51:01 +0100

    Add django_audit_log_middleware to installed apps

--- config/settings/base.py
+++ config/settings/base.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "django_audit_log_middleware",
     # "dal",
     # "dal_select2",
     "sass_processor",
