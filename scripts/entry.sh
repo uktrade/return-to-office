0f7bc4b26690912d71c6790cbff1b2ab72ed9f8b
Ross Miller <github@decodedcollective.com>
2021-06-02 14:29:55 +0100

    Reconfigure to use WSGIServer

--- scripts/entry.sh
+++ scripts/entry.sh
@@ -6,10 +6,4 @@ python manage.py compilescss
 
 python manage.py collectstatic --noinput
 
-gunicorn config.wsgi:application \
-  --config config/gunicorn.py \
-  --worker-class gevent \
-  --worker-connections 1000 \
-  --bind 0.0.0.0:$PORT \
-  --timeout 300 \
-  --log-file -
+python config/app.py
