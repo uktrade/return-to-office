aad83bfaac685e7c6810a9e19b388c60215d3cff
Ross Miller <github@decodedcollective.com>
2021-09-15 16:55:05 +0100

    Use f string rather than format

--- main/forms_pra.py
+++ main/forms_pra.py
@@ -65,11 +65,7 @@ class PRAFormInitial(forms.Form):
 
         if not User.get_by_email(email):
             raise forms.ValidationError(
-                mark_safe(
-                    (
-                        "User not found. If this is their email address, they need to go to <a href='{0}'>this service</a>. Once they have done this, you will be able to use their email address here."
-                    ).format(reverse("main:index"))
-                )
+                mark_safe(f"User not found. If this is their email address, they need to go to <a href='{reverse("main:index")}'>this service</a>. Once they have done this, you will be able to use their email address here.")
             )
 
 
