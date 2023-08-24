3c53b07f9f93e13ad1482ff041089e90b0a8bce7
Jayesh Patel <jayesh.patel@digital.trade.gov.uk>
2023-08-24 21:28:18 +0100

    Create user-post.sh
    rename post hook command

---
+++ user-post.sh
@@ -0,0 +1,5 @@
+echo "Running post build script"
+pip install -r requirements.txt
+python manage.py migrate --settings=config.settings.build --noinput
+python manage.py compilescss --settings=config.settings.build
+python manage.py collectstatic --settings=config.settings.build --noinput
